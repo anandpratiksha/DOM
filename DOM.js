fed193153a48ae27ec2eecc62b53aaee6c4131ef
pratikshaanand <apratiksha1137@gmail.com>
2021-05-05 03:34:41 +0530

    DOM

--- DOM.js
+++ DOM.js
@@ -1,5 +1,13 @@
+document.addEventListener("click", function(){
+    document.getElementById("mo").innerHTML = "You are Beautiful/Handsome !!!";
+  })
+  
 function query() {
-    document.querySelector("h2").innerHTML = "Hello World!";
+    document.querySelector("h2").innerHTML = "Hello !!";
     document.querySelector(".qs").style.color = "red";
     document.querySelector(".qs").innerHTML = "RED color";
-  }
+}
+function fun() {
+    document.querySelectorAll(".a").innerHTML = "cool";
+}
+
