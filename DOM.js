0220ae36f105795f5b8a9451f31b5b1b289f7caf
pratikshaanand <apratiksha1137@gmail.com>
2021-05-05 04:01:03 +0530

    DOM

--- DOM.js
+++ DOM.js
@@ -2,6 +2,18 @@ document.addEventListener("click", function(){
     document.getElementById("mo").innerHTML = "You are Beautiful/Handsome !!!";
   })
   
+ document.addEventListener("mousemove", add);
+function add() {
+  document.getElementById("number").innerHTML = Math.random();
+}
+function removeHandler() {
+  document.removeEventListener("mousemove", add);
+}
+function myFunction() {
+    var btn = document.createElement("BUTTON");
+    document.body.appendChild(btn);
+  }
+
 function query() {
     document.querySelector("h2").innerHTML = "Hello !!";
     document.querySelector(".qs").style.color = "red";
