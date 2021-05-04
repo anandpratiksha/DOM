6f6028ddf18bcaaa0769a36a1a1faa130b00cedb
pratikshaanand <apratiksha1137@gmail.com>
2021-05-05 02:52:42 +0530

    DOM

---
+++ DOM.js
@@ -0,0 +1,5 @@
+function query() {
+    document.querySelector("h2").innerHTML = "Hello World!";
+    document.querySelector(".qs").style.color = "red";
+    document.querySelector(".qs").innerHTML = "RED color";
+  }
