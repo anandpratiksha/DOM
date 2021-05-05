98e417070eec1d9d20164bfaabd637d6ea1a02c2
pratikshaanand <apratiksha1137@gmail.com>
2021-05-06 01:05:46 +0530

    Dom

--- DOM.js
+++ DOM.js
@@ -16,11 +16,43 @@ function myFunction() {
 }
 
  function appenChild(){
-  var node = document.createElement("LI");
-  var textnode = document.createTextNode("Water");
+  var node = document.createElement("li");
+  var textnode = document.createTextNode("Cold Coffee");
   node.appendChild(textnode);
   document.getElementById("myList").appendChild(node);
  }
+
+ function remoChild(){
+  var list = document.getElementById("list");
+  list.removeChild(list.childNodes[0]);
+}
+
+function myFunction3() {
+  var itm = document.getElementById("myList2").lastChild;
+  var cln = itm.cloneNode(true);
+  document.getElementById("myList1").appendChild(cln);
+}
+
+function myFunction11() {
+  var element  = document.getElementById('ul'); // assuming ul exists
+  var fragment = document.createDocumentFragment();
+  var browsers = ['Firefox', 'Chrome', 'Opera',
+      'Safari', 'Internet Explorer'];
+  
+  browsers.forEach(function(browser) {
+      var li = document.createElement('li');
+      li.textContent = browser;
+      fragment.appendChild(li);
+  });
+  
+  element.appendChild(fragment);
+}
+
+function myFunction1(){
+  var textnode = document.createTextNode("Water");
+  var item = document.getElementById("myList1").childNodes[0];
+  item.replaceChild(textnode, item.childNodes[0]);
+}
 
 function query() {
     document.querySelector("#qs").innerHTML = "Hello !!";
